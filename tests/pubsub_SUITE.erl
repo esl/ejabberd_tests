f74e7cbd5bc21822e77f0fab2743d042ffe3980b
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2013-09-17 11:27:16 +0100

    Add pubsub_SUITE stub

---
+++ tests/pubsub_SUITE.erl
@@ -0,0 +1,52 @@
+-module(pubsub_SUITE).
+-compile(export_all).
+
+-include_lib("escalus/include/escalus.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% Suite configuration
+%%--------------------------------------------------------------------
+
+all() ->
+    [{group, pubsub}].
+
+groups() ->
+    [{pubsub, [], [disco]}].
+
+suite() ->
+    escalus:suite().
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+init_per_suite(Config) ->
+    escalus:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    escalus:end_per_suite(Config).
+
+init_per_group(_GroupName, Config) ->
+    escalus:create_users(Config).
+
+end_per_group(_GroupName, Config) ->
+    escalus:delete_users(Config).
+
+init_per_testcase(CaseName, Config) ->
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
+
+%%--------------------------------------------------------------------
+%% Message tests
+%%--------------------------------------------------------------------
+
+disco(_Config) ->
+    ct:fail(not_implemented).
+
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+
