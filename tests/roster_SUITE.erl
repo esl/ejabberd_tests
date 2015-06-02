e5c9541ce2a0eb16a53510feca758bb70d7ea125
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2015-06-02 18:40:52 +0200

    add test for adding a contant to a users roster

--- tests/roster_SUITE.erl
+++ tests/roster_SUITE.erl
@@ -29,20 +29,19 @@
 %%--------------------------------------------------------------------
 
 all() ->
-    [{group, essential}].
+    [
+     {group, read}
+    ,{group, write}
+    ].
 
 groups() ->
-    [{essential, [user_asks_for_roster_after_log_in,
-
-		  user_asks_for_empty_roster,
-
-		  user_asks_for_trivial_nonempty_roster,
-		  user_asks_for_nontrivial_nonempty_roster,
-
-		  user_gets_empty_roster_from_backend,
-                  user_gets_nonempty_roster_from_backend,
-                  user_gets_roster_with_subscription_from_backend]}].
-%% user_gets_roster_with_wrong_subscription_from_backend
+    [
+     {read, [user_asks_for_roster_after_log_in,
+	     user_asks_for_empty_roster,
+	     user_asks_for_trivial_nonempty_roster,
+	     user_asks_for_nontrivial_nonempty_roster]},
+     {write, [user_adds_trivial_contact_to_empty_roster]}
+    ].
 
 suite() ->
     escalus:suite().
@@ -67,10 +66,10 @@ end_per_suite(Config) ->
 
 init_per_group(_GroupName, Config) ->
     %% GIVEN:
-    escalus:create_users(Config, {by_name, [alice]}).
+    escalus:create_users(Config, {by_name, [alice, bob]}).
 
 end_per_group(_GroupName, Config) ->
-    escalus:delete_users(Config, {by_name, [alice]}).
+    escalus:delete_users(Config, {by_name, [alice, bob]}).
 
 init_per_testcase(TestCaseName, Config) ->
     escalus:init_per_testcase(TestCaseName, Config).
@@ -122,38 +121,32 @@ user_asks_for_nontrivial_nonempty_roster(Config) ->
     Roster = [Bob, Carol],
     user_gets_roster_from_http_backend(Config, Roster).
 
-
-user_gets_empty_roster_from_backend(Config) ->
-    user_gets_roster_from_http_backend(Config, []).
-
-user_gets_nonempty_roster_from_backend(Config) ->
-    BobJid = <<"bob@domain">>,
-    Bob = {BobJid, [{<<"jid">>, BobJid}]},
-
-    CarolJid = <<"carol@domain">>,
-    Carol = {CarolJid ,[{<<"jid">>, CarolJid}]},
-
-    Roster = [Bob, Carol],
-    user_gets_roster_from_http_backend(Config, Roster).
-
-user_gets_roster_with_subscription_from_backend(Config) ->
-    BobJid = <<"bob@domain">>,
-    Bob = {BobJid, [{<<"jid">>, BobJid}, {<<"subscription">>, <<"from">>}]},
-
-    Roster = [Bob],
-    user_gets_roster_from_http_backend(Config, Roster).
-
-user_gets_roster_with_wrong_subscription_from_backend(Config) ->
-    BobJid = <<"bob@domain">>,
-    Bob = {BobJid, [{<<"jid">>, BobJid}, {<<"subscription">>, <<"asdf">>}]},
-    Roster = [Bob],
-    try user_gets_roster_from_http_backend(Config, Roster)
-    of
-        _ ->
-            error("Subscription should be one of none | to | from | both")
-    catch
-	error:badarg -> true
-    end.
+user_adds_trivial_contact_to_empty_roster(Config) -> 
+    %% GIVEN:
+    http_roster_server:running(),
+    
+    %% WHEN:
+    escalus:story(
+      Config, [{alice, 1}, {bob, 1}],
+      fun(Alice, Bob) ->
+	      %% GIVEN:
+              user_has_external_roster(Alice, []),
+	      BobJid = escalus_client:short_jid(Bob),
+	      Proplist = [{<<"jid">>, BobJid},
+			      {<<"name">>, <<"bobcat">>},
+			      {<<"groups">>, [<<"wonderland">>,
+					      <<"cryptography">>]}],
+	      InputRoster = [{BobJid, Proplist}],
+	      %% WHEN:
+	      escalus:send(Alice, 
+			   escalus_stanza:roster_add_contact(
+			     Bob,
+			     proplists:get_value(<<"groups">>, Proplist),
+			     proplists:get_value(<<"name">>, Proplist))),
+              %% Then:
+	      OutputRoster = user_fetches_roster(Alice),
+              rosters_equal(InputRoster, OutputRoster)
+      end).
 
 
 %% Auxilliary
