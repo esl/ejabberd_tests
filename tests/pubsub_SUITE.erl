c84eff4566481229585cfa443051d9e261b060f3
astachurski <astachurski@gmail.com>
2015-07-15 18:35:06 +0200

    Corrections to namespaces aded delete node test
    Still problems with random ejabberd behaviour.

--- tests/pubsub_SUITE.erl
+++ tests/pubsub_SUITE.erl
@@ -21,7 +21,10 @@ all() ->
     [{group, pubsub}].
 
 groups() ->
-    [{pubsub, [sequence], [request_to_create_node, request_to_publish_to_node, request_to_subscribe_to_node]}].
+%%    [{pubsub, [sequence], [request_to_create_node, request_to_publish_to_node, request_to_subscribe_to_node]}].
+%%    [{pubsub, [sequence], [request_to_create_node]}].
+    [{pubsub, [sequence], [request_to_create_node, request_to_delete_node]}].
+
 
 
 suite() ->
@@ -31,17 +34,17 @@ suite() ->
 %% Init & teardown
 %%--------------------------------------------------------------------
 
-init_per_group(_GroupName, Config) ->
-    escalus:create_users(Config,{by_name, [alice]}).
-
-end_per_group(_GroupName, Config) ->
-    escalus:delete_users(Config,{by_name, [alice]}).
-
 init_per_suite(Config) ->
     escalus:init_per_suite(Config).
 
 end_per_suite(Config) ->
     escalus:end_per_suite(Config).
+
+init_per_group(_GroupName, Config) ->
+    escalus:create_users(Config,{by_name, [alice]}).
+
+end_per_group(_GroupName, Config) ->
+    escalus:delete_users(Config,{by_name, [alice]}).
 
 init_per_testcase(_TestName, Config) ->
     escalus:init_per_testcase(_TestName, Config).
@@ -59,9 +62,11 @@ end_per_testcase(_TestName, Config) ->
 
 %% ---------------- PUB SUB STANZAS -------------
 
-pubsub_stanza(Children) ->
+
+
+pubsub_stanza(Children, NS) ->
     #xmlel{name = <<"pubsub">>,
-	     attrs = [{<<"xmlns">>, ?NS_PUBSUB} ],
+	     attrs = [{<<"xmlns">>, NS} ],
 	     children = Children  }.
 
 create_specific_node_stanza(NodeName) ->
@@ -107,7 +112,7 @@ sample_publish_node_with_content(NodeName) ->
 
 create_publish_node_content_stanza(NodeName) ->
     PublNode = sample_publish_node_with_content(NodeName),
-    pubsub_stanza([PublNode]).
+    pubsub_stanza([PublNode], ?NS_PUBSUB_OWNER).
 
 
 create_subscribe_node_stanza(NodeName, From) ->
@@ -116,53 +121,98 @@ create_subscribe_node_stanza(NodeName, From) ->
 			       {<<"node">>, NodeName},
 			       {<<"jid">>, escalus_utils:get_jid(From)}]
 		      },
-    pubsub_stanza([SubsrNode]).
+    pubsub_stanza([SubsrNode], ?NS_PUBSUB).
+
+delete_node_stanza(NodeName) ->
+    DelNode = #xmlel{name = <<"delete">>,
+		      attrs = [
+			       {<<"node">>, NodeName}
+			      ]
+		      },
+    pubsub_stanza([DelNode], ?NS_PUBSUB_OWNER).
+
+%% -----------------------------  dummy test ---------------- del me --------
+
+
+
+available(Config) ->
+    escalus:story(Config, [1], fun(Alice) ->
+
+        escalus:send(Alice, escalus_stanza:presence(<<"available">>)),
+        escalus:assert(is_presence, escalus:wait_for_stanza(Alice))
+
+        end).
 
 
 %% ---END---------- PUB SUB STANZAS -------------
+
+-define (DEST_NODE, <<"pubsub.localhost">>).
+-define (DEFAULT_TOPIC, <<"princely_musings">>).
    
 %% XEP0060---8.1.1 Create a node with default configuration ---------------------------
 request_to_create_node(Config) ->
-
-     escalus:story(Config, [{alice,1}],
+    escalus:story(Config, [1],
 		   fun(Alice) ->
-			   PubSubCreate = create_specific_node_stanza(<<"princely_musings">>),
-			   PubSub = pubsub_stanza([PubSubCreate]),
-			   DestinationNode = <<"pubsub.localhost">>,
-			   CreateNodeIq  =  iq_set_get(set, <<"create1">>, DestinationNode, Alice,  PubSub),
-			   escalus:send(Alice, CreateNodeIq),
+			   PubSubCreate = create_specific_node_stanza(?DEFAULT_TOPIC),
+			   PubSub = pubsub_stanza([PubSubCreate], ?NS_PUBSUB),
+			   DestinationNode = ?DEST_NODE,
+			   PubSubCreateIq  =  iq_set_get(set, <<"create1">>, DestinationNode, Alice,  PubSub),
+			   io:format("PubSubCreateIq: ~p~n",[PubSubCreateIq]),
+			   escalus:send(Alice, PubSubCreateIq),
+			   %%escalus:wait_for_stanzas(Alice, 3)
 			   escalus:assert(is_iq_result, escalus:wait_for_stanza(Alice)) %% TODO : enhance this check (Example 131)
 		   end).
 
 
 %% XEP0060---7.1.1 Request to publish to a node -----------------------------------------
 request_to_publish_to_node(Config) ->
-
-     escalus:story(Config, [{alice,1}],
+     escalus:story(Config, [1],
 		   fun(Alice) ->
-			   Publish = create_publish_node_content_stanza(<<"princely_musings">>),
-			   DestinationNode = <<"pubsub.localhost">>,
-			   PublishToNodeIq  =  iq_set_get(set, <<"publish1">>, DestinationNode, Alice,  Publish),
+			   PublishToNode = create_publish_node_content_stanza(?DEFAULT_TOPIC),
+			   DestinationNode = ?DEST_NODE,
+			   PublishToNodeIq  =  iq_set_get(set, <<"publish1">>, DestinationNode, Alice,  PublishToNode),
 			   escalus:send(Alice, PublishToNodeIq),
-			   escalus:assert(is_iq_result, escalus:wait_for_stanza(Alice)) %% TODO : enhance this check (Example 100)
+			   escalus:assert(is_iq_result, escalus:wait_for_stanza(Alice, 5)) %% TODO : enhance this check (Example 100)
 			   %% Detect incoming event message for case with 1-Payload , 2-NoPayload
 		   end).
-
 
     
 %% XEP0060---6.1.1 Subscribe to node request --------------------------------------------
 request_to_subscribe_to_node(Config) ->
 
-     escalus:story(Config, [{alice,1}],
+     escalus:story(Config, [1],
 		   fun(Alice) ->
-			   Subscribe = create_subscribe_node_stanza(<<"princely_musings">>, Alice),
-			   DestinationNode = <<"pubsub.localhost">>,
-			   SubscribeToNodeIq  =  iq_set_get(set, <<"sub1">>, DestinationNode, Alice,  Subscribe),
+			   SubscribeToNode = create_subscribe_node_stanza(?DEFAULT_TOPIC, Alice),
+			   DestinationNode = ?DEST_NODE,
+			   SubscribeToNodeIq  =  iq_set_get(set, <<"sub1">>, DestinationNode, Alice,  SubscribeToNode),
 			   escalus:send(Alice, SubscribeToNodeIq),
-			   escalus:assert(is_iq_result, escalus:wait_for_stanza(Alice)) %% TODO : enhance this check (Example 33)
+			   escalus:wait_for_stanzas(Alice,3)
+			   %%RecvedStanza = escalus:wait_for_stanza(Alice),
+			   %%escalus:assert(is_iq_result, RecvedStanza) %% TODO : enhance this check (Example 33)
 			   %% Detect incoming event message for case with 1-Payload , 2-NoPayload
 		   end).
 
+
+%% XEP0060---8.4.1 Delete node request --------------------------------------------
+request_to_delete_node(Config) ->
+
+     escalus:story(Config, [1], 
+		   fun(Alice) ->
+			   DeleteNode = delete_node_stanza(?DEFAULT_TOPIC),
+			   DestinationNode = ?DEST_NODE,
+			   DeleteNodeIq  =  iq_set_get(set, <<"delete1">>, DestinationNode, Alice,  DeleteNode),
+
+			   io:format("DeleteNodeIqToSend: ~p~n",[DeleteNodeIq]),
+			   escalus:send(Alice, DeleteNodeIq),
+
+			   ResultStanza = escalus:wait_for_stanza(Alice),
+			   io:format("ResultStanza: ~p~n",[ResultStanza]),
+
+			   QueryStanza = escalus_stanza:iq_with_type_id_from(<<"result">>, <<"delete1">>, <<"alice">>),
+			   io:format("QueryStanza: ~p~n",[QueryStanza]),
+			   
+			   escalus:assert(is_iq_result, QueryStanza, ResultStanza) %% enhance this check (Example 157)
+		   end).
 
 
 
